1eb469e554537a7df076dc26f82a854f697146d8
jaimes25 <martin.jaimes7351@gmail.com>
2023-07-17 11:05:16 -0500

    primer commit

---
+++ index.php
@@ -0,0 +1,1 @@
+<?php require-once("index.html") ?
---
+++ portfolio.js
@@ -0,0 +1,10 @@
+// // funcionalidad de desplazar hacia arriba
+// const desplazarArriba = document.getElementById("#desplazarse-hacia-arriba");
+
+// desplazarArriba.addEventListener("click", () => {
+//   window.scrollTo({
+//     top: 0,
+//     left: 0,
+//     behavior: "smooth",
+//   });
+// });
